6a6fd7db740c8cb65d446ec24bb6eca5acd81581
Alison Serafim <89089780+Alisonjs0@users.noreply.github.com>
2024-01-26 16:15:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ function calcular() {
     }
 
     let raiz = Math.sqrt(delta)
-    let baskara1 = ((b*(-1) + raiz) / (2*a)).toFixed(2)
-    let baskara2 = ((b*(-1) - raiz) / (2*a)).toFixed(2)
+    let baskara1 = ((b*(-1) + raiz) / (2*a)).toFixed(1)
+    let baskara2 = ((b*(-1) - raiz) / (2*a)).toFixed(1)
 
     res.innerHTML = `O valor de delta equivale a ${delta}.<br> `
     res.innerHTML += `E as raizes da equação equivalem a ${baskara1} e ${baskara2}`
