452fb19e58929e705c394c13407204856bd78b76
Alison Serafim <89089780+Alisonjs0@users.noreply.github.com>
2024-01-26 15:52:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function calcular() {
+    let txta = document.getElementById('a')
+    let txtb = document.getElementById('b')
+    let txtc = document.getElementById('c')
+    let res = document.getElementById('res')
+    let a = Number(txta.value)
+    let b = Number(txtb.value)
+    let c = Number(txtc.value)
+    
+    if (isNaN(a) || isNaN(b) || isNaN(c)) {
+        window.alert('Verfirique se os valores são validos')
+        return
+    }
+
+    let delta = b**2 - 4*a*c
+
+    if (delta < 0) {
+        window.alert('A equação não possui raizes reais')
+        return
+    }
+
+    let raiz = Math.sqrt(delta)
+    let baskara1 = ((b*(-1) + raiz) / (2*a)).toFixed(2)
+    let baskara2 = ((b*(-1) - raiz) / (2*a)).toFixed(2)
+
+    res.innerHTML = `As raizes da equação equivalem a ${baskara1} e ${baskara2}`
+}
